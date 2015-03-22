43e62d23b44e785fb38bb397809e32488254774f
Shawak <maxi.nussbaum@googlemail.com>
2015-03-22 18:58:42 +0100

    added webserver&webuser

---
+++ src/shawlib/WebServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShawLib
+{
+    public class WebServer
+    {
+        HttpListener listener;
+        Func<HttpListenerRequest, string> method;
+
+        /// <summary>
+        /// Initalize like new WebServer(answer, "http://*:80/name/");
+        /// </summary>
+        /// <param name="responseMethod">Function which  handles the request</param>
+        /// <param name="prefixes">Urls where the webserver will be accessable on</param>
+        public WebServer(Func<HttpListenerRequest, string> responseMethod, params string[] prefixes)
+        {
+            method = responseMethod;
+            listener = new HttpListener();
+
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException("WebServer is not supported on this operating system");
+
+            foreach (var prefix in prefixes)
+                listener.Prefixes.Add(prefix);
+
+            listener.Start();
+        }
+
+        public void Run()
+        {
+            new Thread(threadRun).Start();
+        }
+
+        void threadRun()
+        {
+            while (listener.IsListening)
+            {
+                try
+                {
+                    new Task((c) =>
+                    {
+                        var context = (HttpListenerContext)c;
+
+                        try
+                        {
+                            var ret = method(context.Request);
+                            var buffer = Encoding.UTF8.GetBytes(ret);
+                            context.Response.ContentLength64 = buffer.Length;
+                            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                        }
+                        catch (HttpListenerException)
+                        {
+                            // throws if someone exit page during loading
+                        }
+                        finally
+                        {
+                            context.Response.Close();
+                        }
+                    }, listener.GetContext()).Start();
+                }
+                catch (HttpListenerException)
+                {
+                    // throws if shutting down the webserver because listener.GetContext() aborts
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            listener.Stop();
+            listener.Close();
+        }
+    }
+}
---
+++ src/shawlib/WebUser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ShawLib
+{
+    public struct PostData
+    {
+        public string Name;
+        public string Value;
+    }
+
+    public class WebUser
+    {
+        CookieContainer ck;
+        List<PostData> postDatas;
+
+        public string Url { get; private set; }
+
+        public WebUser()
+        {
+            ck = new CookieContainer();
+            postDatas = new List<PostData>();
+        }
+
+        public void Add(string attr, string val)
+        {
+            var postData = new PostData() { Name = attr, Value = val };
+            postDatas.Add(postData);
+        }
+
+        public string RequestString(string url, string referer = null)
+        {
+            var ret = RequestBytes(url, referer);
+            return ret != null ? Encoding.UTF8.GetString(ret) : null;
+        }
+
+        public byte[] RequestBytes(string url, string referer = null, string postData = null)
+        {
+            var request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.CookieContainer = ck;
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            request.Timeout = 5 * 1000;
+
+            if (postDatas.Count > 0)
+            {
+                request.Method = WebRequestMethods.Http.Post;
+
+                var first = true;
+                var sb = new StringBuilder();
+                foreach (var data in postDatas)
+                {
+                    sb.Append((first ? "" : "&") + WebUtility.UrlEncode(data.Name) + "=" + WebUtility.UrlEncode(data.Value));
+                    if (first)
+                        first = false;
+                }
+                var post = Encoding.UTF8.GetBytes(sb.ToString());
+                request.ContentLength = post.Length;
+                using (var stream = request.GetRequestStream())
+                    stream.Write(post, 0, post.Length);
+                postDatas.Clear();
+            }
+            else
+                request.Method = WebRequestMethods.Http.Get;
+
+            if (!String.IsNullOrEmpty(referer))
+                request.Referer = referer;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            using (var memStream = new MemoryStream())
+            {
+                Url = response.ResponseUri.ToString();
+                stream.CopyTo(memStream);
+                return memStream.ToArray();
+            }
+        }
+    }
+}
